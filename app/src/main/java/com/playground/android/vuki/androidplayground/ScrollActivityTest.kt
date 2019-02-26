4636dc58e6a7ff5f6d5195111108a6ed5d6c926c
Matej Vukosav <vuki146@gmail.com>
2019-02-26 17:59:21 +0100

    Add scroll view example

---
+++ app/src/main/java/com/playground/android/vuki/androidplayground/ScrollActivityTest.kt
@@ -0,0 +1,12 @@
+package com.playground.android.vuki.androidplayground
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ScrollActivityTest : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_test)
+    }
+}
